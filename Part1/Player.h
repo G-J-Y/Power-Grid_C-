71780d3e9c8d57a9c9dafacf7f80440d4885e266
Xiaojnconcordia <43525888+Xiaojnconcordia@users.noreply.github.com>
2019-03-27 11:14:48 -0400

    Add files via upload

---
+++ Part1/Player.h
@@ -0,0 +1,93 @@
+#ifndef PLAYER_H_
+#define PLAYER_H_
+#include <string>
+#include "House.h"
+#include "PowerPlant.h"
+#include "Resources.h"
+#include "mapLoader.h"
+
+using namespace std;
+
+class Player {
+private:
+	//basic data
+	string name;
+	int money;
+	int numOfCity;
+	int numOfPowerPlant;
+	int numOfPoweredCities;
+	PowerPlant powerPlant[3];
+
+	//Resources
+	int coalNum;
+	int oilNum;
+	int garbageNum;
+	int uraniumNum;
+
+	//status of auction
+    bool roundStatus;
+    bool auction;
+    bool bought;
+    int auctionPrice;
+
+	House house;
+
+
+public:
+	Player();
+
+	//set methods
+	void setMoney(int);
+	void setNumOfCity(int);
+	void setNumOfPowerPlant(int);
+	void setNumOfPoweredCities(int);
+	void setCoalNum(int);
+	void setOilNum(int);
+	void setGarbageNum(int);
+	void setUraniumNum(int);
+	void setAuction(bool);
+	void setName(string);
+    void setRoundStatus(bool);
+    void setBought(bool);
+    void setPowerPlant(PowerPlant,int);
+	void setHouse(House h) {
+		house = h;
+	}
+
+	//get methods
+	int getMoney();
+	int getNumOfCity();
+	int getNumOfPowerPlant();
+	int getNumOfPoweredCities();
+	int getCoalNum();
+	int getOilNum();
+	int getGarbageNum();
+	int getUraniumNum();
+	bool getAuction();
+	int getPrice();
+	string getName() const;
+    bool getRoundStatus();
+    bool getBought();
+	PowerPlant getPowerPlant(int);
+
+	//add methods
+	void addNumOfPoweredCities(int);
+
+	//decrease methods
+	void deCoalNum(int);
+	void deOilNum(int);
+	void deGarbageNum(int);
+	void deUraniumNum(int);
+
+	//print information
+	string toString();
+	static void printPlayerPossession(Player*, int);
+
+	void buyResources(Resources*res);
+	void building(Graph* graph, int &step,int numOfPlayer);
+
+};
+
+#endif // !PLAYER_H__H_
+#pragma once
+
---
+++ Part1/player.cpp
@@ -0,0 +1,612 @@
+#include <iostream>
+#include "Player.h"
+#include <string>
+
+
+using namespace std;
+
+// default 
+Player::Player() {
+	name = "";
+	money = 50;
+	numOfCity = 0;
+	numOfPowerPlant = 0;
+	numOfPoweredCities = 0;
+	//powerPlant;
+	coalNum = 0;
+	oilNum = 0;
+	garbageNum = 0;
+	uraniumNum = 0;
+	auction = true;
+	auctionPrice = 0;
+	roundStatus = true;
+	bought = false;
+}
+
+//set method: change value
+void Player::setMoney(int m) {
+	money = m;
+}
+//set method: change value
+void Player::setNumOfCity(int c) {
+	numOfCity = c;
+}
+//set method: change value
+void Player::setNumOfPowerPlant(int p) {
+	numOfPowerPlant = p;
+}
+//set method: change value
+void Player::setNumOfPoweredCities(int pc){
+	numOfPoweredCities = pc;
+}
+//set method: change value
+void Player::setCoalNum(int c) {
+	coalNum = c;
+}
+//set method: change value
+void Player::setOilNum(int o) {
+	oilNum = o;
+}
+//set method: change value
+void Player::setGarbageNum(int g) {
+	garbageNum = g;
+}
+//set method: change value
+void Player::setUraniumNum(int u) {
+	uraniumNum = u;
+}
+//set method: change value
+void Player::setAuction(bool b) {
+	auction = b;
+}
+//set method: change value
+void Player::setRoundStatus(bool b) {
+	roundStatus = b;
+}
+//set method: change value
+void Player::setBought(bool b) {
+	bought = b;
+}
+//set method: change value
+void Player::setPowerPlant(PowerPlant p,int n) {
+	powerPlant[n] = p;
+}
+//get method: get value
+int Player::getMoney(void) {
+	return money;
+}
+//get method: get value
+int Player::getNumOfCity(void) {
+	return numOfCity;
+}
+//get method: get value
+int Player::getNumOfPowerPlant(void) {
+	return numOfPowerPlant;
+}
+//get method: get value
+int Player::getNumOfPoweredCities(){
+	return numOfPoweredCities;
+}
+//get method: get value
+int Player::getCoalNum(void) {
+	return coalNum;
+}
+//get method: get value
+int Player::getOilNum(void) {
+	return oilNum;
+}
+//get method: get value
+int Player::getGarbageNum(void) {
+	return garbageNum;
+}
+//get method: get value
+int Player::getUraniumNum(void) {
+	return uraniumNum;
+}
+//get method: get value
+bool Player::getAuction() {
+	return auction;
+}
+//get method: get value
+bool Player::getRoundStatus() {
+	return roundStatus;
+}
+//get method: get value
+bool Player::getBought() {
+	return bought;
+}
+//get method: get value
+int Player::getPrice() {
+	return auctionPrice;
+}
+//get method: get value
+PowerPlant Player::getPowerPlant(int n) {
+	return powerPlant[n];
+}
+
+
+void Player::setName(string n) {
+	name = n;
+}
+
+string Player::getName() const{
+	return name;
+}
+
+// print the data of players
+string Player::toString() {
+	string print = "Overview card for Player: " + name + "\n->Money: " + to_string(money);
+	print += "coal: " + to_string(coalNum) + " | oil: " + to_string(oilNum) + "  | garbage: " + to_string(garbageNum) + " | uranium: " + to_string(uraniumNum) + "\n";
+	print += "->Num of City: " + to_string(numOfCity) + " | Num Of PoweredCity: " + to_string(numOfPoweredCities)+ "\n";
+	print += "->Cards:\n";
+	for (int i = 0; i < numOfPowerPlant; i++) {
+		print += "  " + powerPlant[i].toString()+"\n";
+	}
+	print += "\n";
+	return print;
+}
+
+void Player::printPlayerPossession(Player* players, int n){
+	for(int i = 0; i<n;i++){
+    cout << players[i].toString() << endl;
+	}
+}
+
+void Player::buyResources(Resources *res) {
+
+	std::cout << "[INFO] " <<name << ", it's your turn now!" << std::endl;
+	std::cout << toString();
+	std::cout << std::endl;
+
+	std::cout << "[INFO] Here is the Resource Market:" << std::endl;
+
+	for (int i = 0; i < 12; i++) {
+		std::cout << res[i].toString() << std::endl;
+	}
+
+	int coalMax = 0;
+	int oilMax = 0;
+	int garbageMax = 0;
+	int uraniumMax = 0;
+	int hybridMax = 0;
+
+	for (int i = 0; i < numOfPowerPlant; i++) {
+		if (powerPlant[i].getTypeName() == "coal") {
+			coalMax += powerPlant[i].getNeed() * 2;
+		}
+		if (powerPlant[i].getTypeName() == "oil") {
+			oilMax += powerPlant[i].getNeed() * 2;
+		}
+		if (powerPlant[i].getTypeName() == "garbage") {
+			garbageMax += powerPlant[i].getNeed() * 2;
+		}
+		if (powerPlant[i].getTypeName() == "uranium") {
+			uraniumMax += powerPlant[i].getNeed() * 2;
+		}
+		if (powerPlant[i].getTypeName() == "hybrid") {
+			hybridMax += powerPlant[i].getNeed() * 2;
+		}
+	}
+
+	while (true)
+	{
+		//Coal Turn
+		while (true)
+		{
+			int inputNum;
+			std::cout << std::endl;
+			std::cout << "[PROMPT] How many coal do you want to buy? (Enter 0 to skip) " << std::endl;
+			std::cout << "[ENTER] ";
+			std::cin >> inputNum;
+			std::cout << std::endl;
+			if (inputNum == 0) {
+				break;
+			}
+			//Check if Player's inventory is full
+			if (inputNum > coalMax + hybridMax - coalNum + ((oilMax - oilNum) < 0 ? (oilMax - oilNum) : 0)) {
+				std::cout << "[ERROR] You do not have enough space! Please enter a smaller numeber."<<std::endl;
+				continue;
+			}
+
+			//Check if Player's Money and Market's inventory is enough
+			
+			//two temp variable
+			int cost = 0;		
+			int tempAmount = 0;
+			
+			for (int i = 0; i < 12; i++) {
+				int left = res[i].getCoal();					//left is the resources left in the marketS
+				if (left == 0) {
+					continue;
+				}
+				if (left >= inputNum - tempAmount) {
+					cost += res[i].getPrice() * (inputNum - tempAmount);
+					tempAmount = inputNum;
+					break;
+				}
+				else if (left < inputNum - tempAmount) {
+					cost += res[i].getPrice() * left;
+					tempAmount += left;
+				}
+			}
+
+			if (tempAmount < inputNum) {
+				std::cout << "[ERROR] Coal is out of stock. Please enter a smaller number!" << std::endl;
+				continue;
+			}
+			if (cost > money) {
+				std::cout << "[ERROR] You do not have enough money. Please enter a smaller number!" << std::endl;
+				continue;
+			}
+
+			//pay money
+			money -= cost;
+
+			tempAmount = 0;
+			for (int i = 0; i < 12; i++) {
+
+				int left = res[i].getCoal();
+
+				if (left == 0) {
+					continue;
+				}
+				if (left >= inputNum - tempAmount) {
+					res[i].setCoal(left - (inputNum - tempAmount));
+					break;
+				}
+				else if (left < inputNum - tempAmount) {
+					tempAmount += left;
+					res[i].setCoal(0);
+				}
+			}
+			//get resources
+			coalNum = inputNum;
+
+			std::cout << "[SUCCESS] " <<inputNum << " coal cost " << cost << " Elektro. You have " << money << " Elektro now." << std::endl;
+			
+			break;
+		}
+		
+		//Oil Turn
+		while (true)
+		{
+			int inputNum;
+			std::cout << std::endl;
+			std::cout << "[PROMPT] How many oil do you want to buy? (Enter 0 to skip)"<< std::endl;
+			std::cout << "[ENTER] ";
+			std::cin >> inputNum;
+			std::cout << std::endl;
+			if (inputNum == 0) {
+				break;
+			}
+			//Check if Player's inventory is vaild
+
+			if (inputNum > oilMax + hybridMax - oilNum + ((coalMax - coalNum) < 0 ? (coalMax - coalNum) : 0)) {
+				std::cout << "[ERROR] You do not have enough space! Please enter a smaller numeber.";
+				continue;
+			}
+
+
+			//Check if Player's Money and Market's inventory is enough
+
+			//two temp variable
+			int cost = 0;
+			int tempAmount = 0;
+
+			for (int i = 0; i < 12; i++) {
+				int left = res[i].getOil();
+				if (left == 0) {
+					continue;
+				}
+				if (left >= inputNum - tempAmount) {
+					cost += res[i].getPrice() * (inputNum - tempAmount);
+					tempAmount = inputNum;
+					break;
+				}
+				else if (left < inputNum - tempAmount) {
+					cost += res[i].getPrice() * left;
+					tempAmount += left;
+				}
+			}
+
+			if (tempAmount < inputNum) {
+				std::cout << "[ERROR] Oil is out of stock. Please enter a smaller number!" << std::endl;
+				continue;
+			}
+			if (cost > money) {
+				std::cout << "[ERROR] You do not have enough money. Please enter a smaller number!" << std::endl;
+				continue;
+			}
+
+			//pay money
+			money -= cost;
+
+			tempAmount = 0;
+			for (int i = 0; i < 12; i++) {
+
+				int left = res[i].getOil();
+
+				if (left == 0) {
+					continue;
+				}
+				if (left >= inputNum - tempAmount) {
+					res[i].setOil(left - (inputNum - tempAmount));
+					break;
+				}
+				else if (left < inputNum - tempAmount) {
+					tempAmount += left;
+					res[i].setOil(0);
+				}
+			}
+			//get resources
+			oilNum = inputNum;
+
+			std::cout << "[SUCCESS]" << inputNum << " oil cost " << cost << " Elektro. You have " << money << " Elektro now." << std::endl;
+			break;
+		}
+		
+		//Garbage Turn
+		while (true)
+		{
+			int inputNum;
+			std::cout << std::endl;
+			std::cout << "[PROMPT] How many garbage do you want to buy? (Enter 0 to skip)" << std::endl;
+			std::cout << "[ENTER] ";
+			std::cin >> inputNum;
+			std::cout << std::endl;
+			if (inputNum == 0) {
+				break;
+			}
+			//Check if Player's inventory is vaild
+
+			if (inputNum > garbageMax ) {
+				std::cout << "[ERROR] You do not have enough space! Please enter a smaller numeber.";
+				continue;
+			}
+
+
+			//Check if Player's Money and Market's inventory is enough
+
+			//two temp variable
+			int cost = 0;
+			int tempAmount = 0;
+
+			for (int i = 0; i < 12; i++) {
+				int left = res[i].getGarbage();
+				if (left == 0) {
+					continue;
+				}
+				if (left >= inputNum - tempAmount) {
+					cost += res[i].getPrice() * (inputNum - tempAmount);
+					tempAmount = inputNum;
+					break;
+				}
+				else if (left < inputNum - tempAmount) {
+					cost += res[i].getPrice() * left;
+					tempAmount += left;
+				}
+			}
+
+			if (tempAmount < inputNum) {
+				std::cout << "[ERROR] Garbage is out of stock. Please enter a smaller number!" << std::endl;
+				continue;
+			}
+			if (cost > money) {
+				std::cout << "[ERROR] You do not have enough money. Please enter a smaller number!" << std::endl;
+				continue;
+			}
+
+			//pay money
+			money -= cost;
+
+			tempAmount = 0;
+			for (int i = 0; i < 12; i++) {
+
+				int left = res[i].getGarbage();
+
+				if (left == 0) {
+					continue;
+				}
+				if (left >= inputNum - tempAmount) {
+					res[i].setGarbage(left - (inputNum - tempAmount));
+					break;
+				}
+				else if (left < inputNum - tempAmount) {
+					tempAmount += left;
+					res[i].setGarbage(0);
+				}
+			}
+			//get resources
+			garbageNum = inputNum;
+
+			std::cout << "[SUCCESS]" << inputNum << " garbage cost " << cost << " Elektro. You have " << money << " Elektro now." << std::endl;
+			break;
+		}
+		
+		//Uranium Turn
+		while (true)
+		{
+			int inputNum;
+			std::cout << std::endl;
+			std::cout << "[PROMPT] How many uranium do you want to buy? (Enter 0 to skip)" << std::endl;
+			std::cout << "[ENTER] ";
+			std::cin >> inputNum;
+			std::cout << std::endl;
+			if (inputNum == 0) {
+				break;
+			}
+			//Check if Player's inventory is vaild
+
+			if (inputNum > uraniumMax) {
+				std::cout << "[ERROR] You do not have enough space! Please enter a smaller numeber.";
+				continue;
+			}
+
+
+			//Check if Player's Money and Market's inventory is enough
+
+			//two temp variable
+			int cost = 0;
+			int tempAmount = 0;
+
+			for (int i = 0; i < 12; i++) {
+				int left = res[i].getUranium();
+				if (left == 0) {
+					continue;
+				}
+				if (left >= inputNum - tempAmount) {
+					cost += res[i].getPrice() * (inputNum - tempAmount);
+					tempAmount = inputNum;
+					break;
+				}
+				else if (left < inputNum - tempAmount) {
+					cost += res[i].getPrice() * left;
+					tempAmount += left;
+				}
+			}
+
+			if (tempAmount < inputNum) {
+				std::cout << "[ERROR] Uranium is out of stock. Please enter a smaller number!" << std::endl;
+				continue;
+			}
+			if (cost > money) {
+				std::cout << "[ERROR] You do not have enough money. Please enter a smaller number!" << std::endl;
+				continue;
+			}
+
+			//pay money
+			money -= cost;
+
+			tempAmount = 0;
+			for (int i = 0; i < 12; i++) {
+
+				int left = res[i].getUranium();
+
+				if (left == 0) {
+					continue;
+				}
+				if (left >= inputNum - tempAmount) {
+					res[i].setUranium(left - (inputNum - tempAmount));
+					break;
+				}
+				else if (left < inputNum - tempAmount) {
+					tempAmount += left;
+					res[i].setUranium(0);
+				}
+			}
+			//get resources
+			garbageNum = inputNum;
+
+			std::cout << "[SUCCESS]" << inputNum << " uranium cost " << cost << " Elektro. You have " << money << " Elektro now." << std::endl;
+			break;
+		}
+		break;
+	}
+}
+
+
+void Player::building(Graph* myGameMap, int &step, int numOfPlayer) {
+
+	std::cout << std::endl;
+	printGraph(myGameMap);
+	std::cout << "[INFO]" << name << ", it's your turn!"<<std::endl;
+
+	while (true) {
+		//check the step
+		if (numOfPlayer ==2){
+			if (numOfCity ==10){
+				step =2;
+				std::cout <<"[INFO] It is STEP 2 now!!" <<std::endl;
+			}
+		}
+		if (numOfPlayer==3||numOfPlayer==4||numOfPlayer==5){
+			if(numOfCity==7){
+				step =2;
+				std::cout <<"[INFO] It is STEP 2 now!!" <<std::endl;
+			}
+		}
+		if (numOfPlayer==6){
+			if(numOfCity==6){
+				step =2;
+				std::cout <<"[INFO] It is STEP 2 now!!" <<std::endl;
+			}
+		}
+
+		std::cout << "[PROMPT]" << "Please enter the ID of the city that you want to build.(Enter -1 to skip)" << std::endl;
+		std::cout << "[ENTER] ";
+		int inputNum;
+		std::cin >> inputNum;
+		std::cout << std::endl;
+		
+		if (inputNum == -1) {
+			break;
+		}
+
+		vector<string> owners = myGameMap->getArr()[inputNum].getBase()->getOwners();
+		int size = owners.size();
+
+
+		//check if the city is on the map
+		if (myGameMap->getArr()[inputNum].getHead() == NULL) {
+			cout << "[ERROR] This city is not on the map. Please try another city!!\n";
+			continue;
+		}
+
+		//check if the player have already built one
+		bool isBuilt = false;
+		for (int i = 0; i < size; i++) {
+			if (name == owners[i]) {
+				cout << "[ERROR] You have owned this city. Please try another city!!\n";
+				isBuilt = true;
+				continue;
+			}
+		}
+		if (isBuilt) { continue; }
+
+		//check if there is enough space 
+		if (size >= step) {
+			std::cout << "[ERROR] You can not build plant in this city because there is no more space!" << std::endl;
+			continue;
+		}
+		
+		//check if player has enough money
+		int price = myGameMap->getArr()[inputNum].getBase()->getPrice(size);
+		//add network price
+		price += myGameMap->lowestPathPrice(inputNum,name);
+		if (money < price){
+			std::cout << "[ERROR] You do not have enough money. Please try another city!" << std::endl;
+			continue;
+		}
+
+		myGameMap->getArr()[inputNum].getBase()->addOwner(name);
+		money -= price;
+		numOfCity++;
+		std::cout << "[SUCCESS] You have built a house in "<< myGameMap->getArr()[inputNum].getBase()->getName() << " successfully." << std::endl;
+		std::cout << "[SUCCESS] It cost you  "<< price <<" Elektro. Now you have "<< money<< " Elektro." << std::endl;
+
+	}
+	
+	printGraph(myGameMap);
+}
+
+
+
+//add methods
+void Player::addNumOfPoweredCities(int c){
+	numOfPoweredCities += c;
+}
+
+
+//decrease methods
+void Player::deCoalNum(int c){
+	coalNum -= c;
+}
+void Player::deOilNum(int o){
+	oilNum -= o;
+}
+void Player::deGarbageNum(int g){
+	garbageNum -= g;
+}
+void Player::deUraniumNum(int u){
+	uraniumNum -= u;
+}
