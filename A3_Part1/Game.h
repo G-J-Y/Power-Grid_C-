9f077691e705ced31d49811c57b8835607d85fde
natsume <zhangyanqi2011@gmail.com>
2019-04-11 22:20:57 -0400

    add folder/A3 submission

--- A3_Part1/Game.h
+++ A3_Part1/Game.h
@@ -73,7 +73,7 @@ public:
 
     Game(int numOfPlayer);
 
-	~Game() {};
+	~Game();
 
     void loader();
 
--- A3_Part1/Observer.h
+++ A3_Part1/Observer.h
@@ -1,5 +1,5 @@
 #pragma once
-#include"player.h"
+#include"Player.h"
 class Observer
 {	
 protected:
